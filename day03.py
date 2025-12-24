9732a9d2412c6779163e4bf10ff32a8726b6031a
Matthias Liedtke <mliedtke@gmx.de>
2025-12-24 11:43:52 +0100

    Day 3 part 2

--- day03.py
+++ day03.py
@@ -1,7 +1,11 @@
-sum_a = 0
-for bank in open("inputs/day03.txt").read().split("\n"):
-    first = max(bank[:-1])
+def solve(bank, digits):
+    if digits == 1:
+        return max(bank)
+    first = max(bank[:-digits+1])
     index = bank.index(first)
-    second = max(bank[index+1:])
-    sum_a += int(first + second)
-print(f"a = {sum_a}")
+    tail = solve(bank[index+1:], digits-1)
+    return first + tail
+
+banks = open("inputs/day03.txt").read().split("\n")
+print(f"a = {sum(int(solve(bank, 2)) for bank in banks)}")
+print(f"b = {sum(int(solve(bank, 12)) for bank in banks)}")
