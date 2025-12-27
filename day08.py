a50cd7bf28490cd5d1b63d06af698ea1fb0fe203
Matthias Liedtke <mliedtke@gmx.de>
2025-12-27 22:58:49 +0100

    Day 8 part 2

--- day08.py
+++ day08.py
@@ -11,12 +11,15 @@ for i, a in enumerate(boxes):
 
 heapq.heapify(distances)
 grids = {box: set([box]) for box in boxes}
-for _ in range(1_000):
+for i in range(1, 1_000_000):
     distance, a, b = heapq.heappop(distances)
-    print(f"shortest: {a} -> {b}: {distance}")
     new_grid = grids[a].union(grids[b])
     for point in new_grid:
         grids[point] = new_grid
-unique_grids = set(map(tuple, grids.values()))
-sizes = list(reversed(sorted([len(grid) for grid in unique_grids])))
-print(f"a = {reduce(operator.mul, sizes[:3])}")
+    if i == 1_000:
+        unique_grids = set(map(tuple, grids.values()))
+        sizes = list(reversed(sorted([len(grid) for grid in unique_grids])))
+        print(f"a = {reduce(operator.mul, sizes[:3])}")
+    if len(new_grid) == len(boxes):
+        print(f"b = {a[0] * b[0]}")
+        break
