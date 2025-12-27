9fe4f97df7d301159385631c5a63cd02d6b857b4
Matthias Liedtke <mliedtke@gmx.de>
2025-12-27 17:26:54 +0100

    cleanup

--- day06.py
+++ day06.py
@@ -5,7 +5,8 @@ lines = [l.split(" ") for l in open(filename).read().split("\n")]
 lines = [[e for e in line if e != ""] for line in lines]
 ops = lines[-1]
 nums = [[int(e) for e in line] for line in lines[:-1]]
-print(f"a = {sum(reduce(operator.mul if op == "*" else operator.add, operands) for operands, op in zip(zip(*nums), ops))}")
+merge = lambda op: operator.mul if op == "*" else operator.add
+print(f"a = {sum(reduce(merge(op), operands) for operands, op in zip(zip(*nums), ops))}")
 
 lines = open(filename).read().split("\n")[:-1]
 nums = ["".join(col).strip() for col  in zip(*lines)]
@@ -17,4 +18,4 @@ for num in nums:
     else:
         curr.append(int(num))
 parsed_nums.append(curr)
-print(f"b = {sum(reduce(operator.mul if op == "*" else operator.add, operands) for operands, op in zip(parsed_nums, ops))}")
+print(f"b = {sum(reduce(merge(op), operands) for operands, op in zip(parsed_nums, ops))}")
