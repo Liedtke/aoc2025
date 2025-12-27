74783efebdd5d0e579b65a8365d21eb1bb4ac714
Matthias Liedtke <mliedtke@gmx.de>
2025-12-27 22:05:56 +0100

    Day 7 part 2

--- day07.py
+++ day07.py
@@ -1,14 +1,17 @@
+from collections import defaultdict
+
 grid = open("inputs/day07.txt").read().split("\n")
-beams = set([grid[0].index("S")])
+beams = {grid[0].index("S"): 1}
 splits = 0
 for y, row in enumerate(grid):
-    new_beams = set()
-    for beam in beams:
+    new_beams = defaultdict(int)
+    for beam, count in beams.items():
         if row[beam] == "^":
             splits += 1
-            new_beams.add(beam-1)
-            new_beams.add(beam+1)
+            new_beams[beam-1] += count
+            new_beams[beam+1] += count
         else:
-            new_beams.add(beam)
+            new_beams[beam] += count
     beams = new_beams
 print(f"a = {splits}")
+print(f"b = {sum(beams.values())}")
